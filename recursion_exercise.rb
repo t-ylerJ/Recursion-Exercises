e06b3805d5530e4af1099600c048c7614b8b4d07
shjang1025 <shjang1025@gmail.com>
2024-01-24 11:19:51 -0800

    recursive sum of array update

--- recursion_exercise.rb
+++ recursion_exercise.rb
@@ -9,3 +9,17 @@ def array_sum(arr)
   arr.each{ |ele| sum += ele}
   return sum 
 end
+
+def array_sum2(arr)
+  #base case
+  if arr.length > 0
+  #inductive 
+  
+    array_sum2(arr[0]) + array_sum2(arr[1..-1])
+  end
+
+end
+
+# [1] - 1 
+# [1,2] - 3 = 1 + 2 = prev.sol + arr[-1]
+# [1,2,3] - 6 = 3 + 3 = prev.sol + arr[-1]
